f44474bc8fcebe53a1d50e14d74ee4efedd12dac
yeshouzzz <814536023@qq.com>
2020-12-08 19:55:10 +0800

    shishi

--- miao/lodash/yeshouzz-lodash.js
+++ miao/lodash/yeshouzz-lodash.js
@@ -1,4 +1,26 @@
 var yeshouzz = {
+  chunk: function (array, size) {
+    var a = []
+    var x = -1
+    for (var i = 0; i < array.length; i++) {
+      if (i % size == 0) {
+        a.push([array[i]])
+        x++
+      } else {
+        a[x].push(array[i])
+      }
+    }
+    return a
+  },
+  compact: function (array) {
+    var a = []
+    for (var i = 0; i < array.length; i++) {
+      if (array[i]) {
+        a.push([array[i]])
+      }
+    }
+    return a
+  },
 
 
 

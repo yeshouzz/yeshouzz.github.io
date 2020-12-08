3b45eddae113e93da5d76b2331aeb93f4fa64098
yeshouzzz <814536023@qq.com>
2020-12-08 20:54:36 +0800

    kankan

--- miao/lodash/yeshouzz-lodash.js
+++ miao/lodash/yeshouzz-lodash.js
@@ -16,11 +16,41 @@ var yeshouzz = {
     var a = []
     for (var i = 0; i < array.length; i++) {
       if (array[i]) {
-        a.push([array[i]])
+        a.push(array[i])
       }
     }
     return a
   },
+  difference: function (array, valuse) {
+    var a = []
+    for (var i = 0; i < array.length; i++) {
+      for (var j = 0; j < valuse.length; j++) {
+        if (array[i] == valuse[j]) {
+          break
+        }
+        if (j == valuse.length - 1) {
+          a.push(array[i])
+        }
+      }
+    }
+    return a
+  },
+  drop: function (array, n) {
+    if (n == 0) return array
+    if (!n) n = 1
+    while (n > 0) {
+      array.shift()
+      n--
+    }
+    return array
+  },
+
+
+
+
+
+
+
 
 
 

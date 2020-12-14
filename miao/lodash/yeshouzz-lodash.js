6250eef1b32b29ae6dab40f0978ea56da93fc976
yeshouzzz <814536023@qq.com>
2020-12-14 22:04:41 +0800

    up

--- miao/lodash/yeshouzz-lodash.js
+++ miao/lodash/yeshouzz-lodash.js
@@ -114,38 +114,93 @@ var yeshouzz = {
     var flDeep = function (arr, depth) {
       arr.forEach((it) => {
         if (Array.isArray(it) && depth > 0) {
-          flDeep(it, depth--)
+          a.push(...flDeep(it, --depth))
         } else {
           a.push(it)
         }
       })
     }
-    flDeep(arr, depth)
-    return a
   },
-  indexOf: function (array, value, fromIndex = 1) {
+  indexOf: function (array, value, fromIndex = 1) {//考虑多次循环的数以及负数，记录相等的个数x，fromIndex % x
+    var a = 0
+    var b = []
     for (var i = 0; i < array.length; i++) {
       if (array[i] == value) {
-        fromIndex--
-        if (fromIndex == 0) {
-          break
-        }
+        b.push(i)
+        a++
       }
     }
-    return i
+    if (fromIndex > 0) {
+      var c = fromIndex % (a + 1)
+      if (c == 0) {
+        return b[b.length - 1]
+      }
+      return b[c - 1]
+    }
+    if (fromIndex < 0) {
+      var c = -fromIndex % (a + 1)
+      if (c == 0) {
+        return b[0]
+      }
+      return b[b.length - c]
+    }
   },
   initial: function (array) {
     array.pop()
     return array
   },
-
-
-
-
-
-
-
-
+  intersection: function (ary, ...array) {
+    var a = []
+    var y = []
+    var b = array.length
+    for (var i = 0; i < b; i++) {
+      a.push(...values[i])
+    }
+    for (var i = 0; i < ary.length; i++) {
+      var x = 0
+      for (var j = a.length - 1; 0 <= j; j--) {
+        if (ary[i] = a[j]) {
+          x++
+        }
+      }
+      if (x == b) {
+        y.push(ary[i])
+      }
+    }
+    return y
+  },
+  join: function (array, separator = ',') {
+    var a = array[0]
+    for (var i = 1; i < array.length; i++) {
+      a = a + separator + array[i]
+    }
+    return a
+  },
+  last: function (array) {
+    return array[array.length - 1]
+  },
+  lastIndexOf: function (array, value, fromIndex = array.length - 1) {
+    var a = []
+    for (var i = 0; i < array.length; i++) {
+      if (array[i] == value) {
+        a.push(i)
+      }
+    }
+    if (fromIndex > 0) {
+      var b = fromIndex % a.length
+      if (b == 0) {
+        return a[0]
+      }
+      return a[length - b]
+    }
+    if (fromIndex < 0) {
+      var b = -fromIndex % a.length
+      if (b == 0) {
+        return a[length - 1]
+      }
+      return a[b - 1]
+    }
+  },
 
 
 
